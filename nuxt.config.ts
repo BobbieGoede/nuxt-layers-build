88a93a75a969ed0919e5a2e65669958d6254d7e2
Bobbie Goede <bobbiegoede@gmail.com>
2023-03-12 11:18:37 +0100

    chore: setup reproduction

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,15 @@
+if (process.env.LAYER === "local") console.log("");
+
+const localLayer = process.env.LAYER === "local";
+const multipleLayers = process.env.MULTIPLE && process.env.LAYER === "remote";
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
+
 export default defineNuxtConfig({
 	extends: [
-		"github:BobbieGoede/nuxt-layers-build/test-layer",
-		"github:BobbieGoede/nuxt-layers-build/test-layer-setup",
+		localLayer ? "test-layer" : "github:BobbieGoede/nuxt-layers-build/test-layer",
+		...[multipleLayers ? "github:BobbieGoede/nuxt-layers-build/test-layer-setup" : undefined],
+		// github:BobbieGoede/nuxt-basic-project
+		// "github:BobbieGoede/nuxt-layers-build/test-layer-setup",
+		// "github:BobbieGoede/nuxt-i18n-layer-test",
 	],
 });
