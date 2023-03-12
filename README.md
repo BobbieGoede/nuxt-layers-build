5e79b2f4854e49166a88820dfde961208434e556
BobbieGoede <bobbiegoede@gmail.com>
2023-03-12 09:37:09 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nuxt-layers-build
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-c9w1j2)
---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <NuxtLink v-for="link in links" :key="link" :to="link">{{ link }}</NuxtLink>
+    <NuxtPage />
+  </div>
+</template>
+
+<script setup>
+const links = ['/', '/regular-page', '/page-setup'];
+</script>
